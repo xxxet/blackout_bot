e23b6203c742e0e90a212cf7dd17dbebc3bcb396
dmitrykarpenko <dmitrii.kar@gmail.com>
2024-07-30 23:43:20 +0300

    added flask

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 python3 -m venv venv-d
 source ./venv-d/bin/activate
 pip install -r requirements.txt -U
+python3 run_flask.py --port 8080 &
 python3 run_bot.py --token "$1"
---
+++ run_flask.py
@@ -0,0 +1,12 @@
+import argparse
+
+from src.rest.hello import run_server
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--port", help="flask port")
+    args = parser.parse_args()
+    config = vars(args)
+    run_server(int(repr(config.get("port"))))

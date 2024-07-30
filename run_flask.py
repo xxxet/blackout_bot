a9e0eb0b215abc78616d30362f8d03b4848ae1f0
dmitrykarpenko <dmitrii.kar@gmail.com>
2024-07-31 00:02:44 +0300

    added flask

--- run_flask.py
+++ run_flask.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     parser.add_argument("--port", help="flask port")
     args = parser.parse_args()
     config = vars(args)
-    run_server(int(repr(config.get("port"))))
+    run_server(int(str(config.get("port"))))

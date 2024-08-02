f96286b588f8286ade3582b63a13e266f1abe713
dmitrykarpenko <dmitrii.kar@gmail.com>
2024-08-02 16:00:14 +0300

    compatibility with cloud

--- run_flask.py
+++ run_flask.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     parser.add_argument("--port", help="flask port")
     args = parser.parse_args()
     config = vars(args)
-    port = str(config.get("port"))
+    port = str(config.get("PORT"))
     if len(port) == 0:
-        port = str(os.getenv("port"))
+        port = str(os.getenv("PORT"))
     run_server(int(port))

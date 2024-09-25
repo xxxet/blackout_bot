18282ed6ab26f82a42df9e0a2138107c2a58a877
dmitrykarpenko <dmitrii.kar@gmail.com>
2024-09-26 00:26:49 +0300

    updated timefinder tests

--- tests/test_timefinder.py
+++ tests/test_timefinder.py
@@ -1,3 +1,4 @@
+import pytest
 from assertpy import assert_that, soft_assertions
 from freezegun import freeze_time
 
@@ -25,21 +26,21 @@ class TestTimeFinder:
                 "08-19-2024 20:45:00 +0300"
             )
 
-    @freeze_time("08-19-2024 20:45:00 +0300")
-    def test_notify_after_remind_time(self) -> None:
-        """Test notification in 'notify before range'"""
+    @freeze_time("08-19-2024 21:00:00 +0300")
+    def test_notify_zone_starts(self) -> None:
+        """Test notification before 'notify before time'"""
         tf = SqlTimeFinder("group5", config.tz)
         tf.read_schedule()
         remind = tf.find_next_remind_time(15)
         with soft_assertions():
-            assert_that(remind.notify_now).is_equal_to(True)
-            assert_that(remind.new_zone).matches("white")
-            assert_that(remind.old_zone).matches("grey")
+            assert_that(remind.notify_now).is_equal_to(False)
+            assert_that(remind.new_zone).matches("black")
+            assert_that(remind.old_zone).matches("white")
             assert_that(to_dateformat(remind.change_time)).is_equal_to(
-                "08-19-2024 21:00:00 +0300"
+                "08-20-2024 00:00:00 +0300"
             )
             assert_that(to_dateformat(remind.remind_time)).is_equal_to(
-                "08-19-2024 20:45:00 +0300"
+                "08-19-2024 23:45:00 +0300"
             )
 
     @freeze_time("08-19-2024 20:45:00 +0300")
@@ -58,3 +59,23 @@ class TestTimeFinder:
             assert_that(to_dateformat(remind.remind_time)).is_equal_to(
                 "08-19-2024 23:45:00 +0300"
             )
+
+    @pytest.mark.parametrize(
+        "time_to_freeze", ["08-19-2024 20:45:00 +0300", "08-19-2024 20:59:59 +0300"]
+    )
+    def test_notify_in_remind_period(self, time_to_freeze: str) -> None:
+        """Test notification in 'notify before range', notify_now should be True"""
+        tf = SqlTimeFinder("group5", config.tz)
+        tf.read_schedule()
+        with freeze_time(time_to_freeze):
+            remind = tf.find_next_remind_time(15)
+            with soft_assertions():
+                assert_that(remind.notify_now).is_equal_to(True)
+                assert_that(remind.new_zone).matches("white")
+                assert_that(remind.old_zone).matches("grey")
+                assert_that(to_dateformat(remind.change_time)).is_equal_to(
+                    "08-19-2024 21:00:00 +0300"
+                )
+                assert_that(to_dateformat(remind.remind_time)).is_equal_to(
+                    "08-19-2024 20:45:00 +0300"
+                )
